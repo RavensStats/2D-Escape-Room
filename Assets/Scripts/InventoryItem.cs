f8cbd77dd389689063af5b553db8031ba13f84f8
marlene-ber <m.berenger@lancaster.ac.uk>
2026-05-27 14:25:43 +0200

    Added Alessandra's inventory updates, small fixes, some manual integration (copy/pasting when VS code was a pain)

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -4,10 +4,11 @@ public class InventoryItem : MonoBehaviour
 {
     public InventoryBoxManager inventoryManager;
     public Sprite inventoryIcon;
+    public string itemID;
 
     public void PickUpItem()
     {
-        bool added = inventoryManager.AddItem(inventoryIcon);
+        bool added = inventoryManager.AddItem(inventoryIcon, itemID);
 
         if (added)
         {
